6266edf8bbe367ba68bc0fbd69c8fc92a2ebb789
Michał Roziel <137206908+michalroziel@users.noreply.github.com>
2025-12-11 22:12:25 +0100

    Update MQTT broker and topic configuration

--- raspberry_pi/oasis_subscriber.py
+++ raspberry_pi/oasis_subscriber.py
@@ -1,16 +1,18 @@
 import paho.mqtt.client as mqtt
 
-BROKER = "localhost"
-TOPIC = "oasis/soil"
+BROKER = "broker.hivemq.com"
+PORT = 1883
+TOPIC = "esp/moisture"
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc, properties=None):
     print("Connected with result code", rc)
     client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
     print("Topic:", msg.topic, "| Payload:", msg.payload.decode())
 
-client = mqtt.Client()
+client = mqtt.Client(client_id="PiSubscriber", protocol=mqtt.MQTTv5)
+
 client.on_connect = on_connect
 client.on_message = on_message
 
