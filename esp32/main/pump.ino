090dc549b62c65479a2f8bff4764ba9a81a50565
omarq03 <omarmqoul@gmail.com>
2026-02-03 23:41:36 +0100

    Update pump.ino

--- esp32/main/pump.ino
+++ esp32/main/pump.ino
@@ -1,33 +1,20 @@
 // main/pump.ino
-// Simple Variable Speed Pump & Relay
-// the pump we are using is 12V DC
+// pump 12V 
 
-// and : using MAX14870 Polulu Motor Driver
+const int PIN_MOTOR_PWM = 25; //   speed 
+const int PIN_MOTOR_DIR = 27; // direction 
 
-// - -  PIN DEFINITIONS - - 
-// PIN_RELAY is defined in main.ino
-
-const int PIN_MOTOR_PWM = 25; // Speed : MAX14870 PWM
-const int PIN_MOTOR_DIR = 27; // Direction  : MAX14870 DIR
-
-// - - set the  pump - - 
-// Speed: 0 (Halt) to 255 (100 %)
-
-const int PUMP_SPEED = 255; 
+const int PUMP_SPEED = 255;  //max speed
 
 void pump_init() {
-  // Setup Pins
   pinMode(PIN_MOTOR_DIR, OUTPUT);
   pinMode(PIN_RELAY, OUTPUT);
   
   // PWM Setup: 20kHz, 8-bit resolution
   ledcAttach(PIN_MOTOR_PWM, 20000, 8);
 
-  // --- SAFETY INITIALIZATION ---
-  // Since your relay is Active LOW (LOW = ON), we write HIGH to start it OFF.
-  digitalWrite(PIN_RELAY, HIGH); 
+  digitalWrite(PIN_RELAY, HIGH);  // high meanss to start relay as off for safety
   
-  // Ensure Motor Driver is also 0
   digitalWrite(PIN_MOTOR_DIR, LOW); 
   ledcWrite(PIN_MOTOR_PWM, 0);
 
@@ -35,14 +22,14 @@ void pump_init() {
 }
 
 void pump_on() {
-  digitalWrite(PIN_RELAY, LOW);   // relay ON (active LOW)
+  digitalWrite(PIN_RELAY, LOW);   // relay on (active low
   ledcWrite(PIN_MOTOR_PWM, PUMP_SPEED);
   Serial.println("[PUMP] ON");
 }
 
 void pump_off() {
   ledcWrite(PIN_MOTOR_PWM, 0);
-  digitalWrite(PIN_RELAY, HIGH);  // relay OFF
+  digitalWrite(PIN_RELAY, HIGH);  // relay off
   Serial.println("[PUMP] OFF");
 }
 
